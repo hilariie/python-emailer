762b6fccab943fe470093b29e599ff4bd6780bb7
yunghil <holaryc@gmail.com>
2022-03-24 21:51:31 +0100

    updated version number

--- build/lib/python_emailer/tes1.py
+++
@@ -1,4 +0,0 @@
-from email_processing import PyMailer
-login_details = ['holaryc@gmail.com', 'password']
-email_sender = PyMailer('hilary.akpu@e4email.net', login_details, subject='Job Application', message='Find attached my cv')
-email_sender.send_email()
